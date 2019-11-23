17e11681f2d24bdab4b770aa887768e2dac78c78
abderrahman-dz <46720146+abderrahman-dz@users.noreply.github.com>
2019-11-23 19:03:27 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,22 +58,6 @@ client.on('message', message => {
     }
     });
 
-
-
-client.on("message", message => {
-
-            if (message.content.startsWith(prefix + "bc")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : عدد الاعضاء المستلمين`); 
- message.delete(); 
-};     
-});
-
 const developers = ["278650446090993665"] // ايدي صاحب البوت
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
